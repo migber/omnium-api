09fb9279bb313333a2461db4ec9364a7bcc4f7cf
Migle Beresineviciute <migle.brs@gmail.com>
2018-03-13 00:28:16 +0200

    implemented without auth

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,40 +6,42 @@ const router = express.Router()
 const bodyParser = require('body-parser')
 const request = require('request')
 
+const routesWithoutAuth = ['/api/users/exists', '/api/users/creteLogged']
 router.use(bodyParser.urlencoded({ extended: false }))
 router.use(express.static('public'))
 
 
 router.use((req, res, next) => {
   console.log('token validation')
-  console.log(req)
-  // check header or url parameters or post parameters for token
-  const bearerToken = req.body.token || req.query.token || req.headers.authorization
-  console.log(bearerToken)
-  const token = bearerToken.replace('Bearer ', '')
-  console.log(token)
-  const data = {
-    access_token: token,
-  }
-  console.log(data)
-  // decode token
-  if (token) {
-    console.log(token)
-    request.post('https://www.googleapis.com/oauth2/v3/tokeninfo', { form: data }, (error, response, body) => {
-      // console.log(response)
-      if (response.statusCode === 200) {
-        console.log(body)
-        next()
-      } else {
-        res.status(response.statusCode)
-        res.send(response.body)
-      }
-    })
+  console.log(req.url)
+  if (routesWithoutAuth.includes(req.url)) {
+    next()
   } else {
-    return res.status(403).send({
-      success: false,
-      message: 'No token provided.',
-    })
+    const bearerToken = req.body.token || req.query.token || req.headers.authorization
+    console.log(bearerToken)
+    const token = bearerToken.replace('Bearer ', '')
+    console.log(token)
+    const data = {
+      access_token: token,
+    }
+    console.log(data)
+    // decode token
+    if (token) {
+      console.log(token)
+      request.post('https://www.googleapis.com/oauth2/v3/tokeninfo', { form: data }, (error, response) => {
+        if (response.statusCode === 200) {
+          next()
+        } else {
+          res.status(response.statusCode)
+          res.send(response.body)
+        }
+      })
+    } else {
+      return res.status(403).send({
+        success: false,
+        message: 'No token provided.',
+      })
+    }
   }
 })
 
